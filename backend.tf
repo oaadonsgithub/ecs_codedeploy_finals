9807a5798c8c2bbf0273e33166bd701be2f4739b
oaadonsgithub <77804816+oaadonsgithub@users.noreply.github.com>
2025-05-04 16:03:46 -0700

    Initial commit for CodeDeploy automation_workflow_6

---
+++ backend.tf
@@ -0,0 +1,16 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+# The block below configures Terraform to use the 'remote' backend with HCP Terraform.
+# For more information, see https://www.terraform.io/docs/backends/types/remote.html
+terraform {
+  cloud {
+    organization = "oaa_dons"
+
+    workspaces {
+      name = "terraform-github-actions"
+    }
+  }
+
+  required_version = ">= 1.1.2"
+}
--- main.tf
+++ main.tf
@@ -1,82 +1,41 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-    }
-  }
-  required_version = ">= 1.1.0"
+# The following configuration uses a provider which provisions [fake] resources
+# to a fictitious cloud vendor called "Fake Web Services". Configuration for
+# the fakewebservices provider can be found in provider.tf.
+#
+# After running the setup script (./scripts/setup.sh), feel free to change these
+# resources and 'terraform apply' as much as you'd like! These resources are
+# purely for demonstration and created in HCP Terraform, scoped to your HCP Terraform
+# user account.
+#
+# To review the provider and documentation for the available resources and
+# schemas, see: https://registry.terraform.io/providers/hashicorp/fakewebservices
+#
+# If you're looking for the configuration for the remote backend, you can find that
+# in backend.tf.
 
-  cloud {
-    organization = "oaa_dons"
 
-    workspaces {
-      name = "terraform-github-actions"
-    }
-  }
+resource "fakewebservices_vpc" "primary_vpc" {
+  name       = "Primary VPC"
+  cidr_block = "0.0.0.0/1"
 }
 
-provider "aws" {
-  region = "us-west-1"
+resource "fakewebservices_server" "servers" {
+  count = 2
+
+  name = "Server ${count.index + 1}"
+  type = "t2.micro"
+  vpc  = fakewebservices_vpc.primary_vpc.name
 }
 
-resource "random_pet" "sg" {}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+resource "fakewebservices_load_balancer" "primary_lb" {
+  name    = "Primary Load Balancer"
+  servers = fakewebservices_server.servers[*].name
 }
 
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
-}
-
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
+resource "fakewebservices_database" "prod_db" {
+  name = "Production DB"
+  size = 256
 }
