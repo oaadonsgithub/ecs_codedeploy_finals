cb04406b39d79f1d124e9036f2d7eb061da8f3b9
oaadonsgithub <77804816+oaadonsgithub@users.noreply.github.com>
2025-05-04 15:36:34 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "oaa_dons"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "terraform-github-actions"
     }
   }
 }
