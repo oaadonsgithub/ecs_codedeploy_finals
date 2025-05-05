47413d10411f9c0e5d6957b3c725f664662f6278
oaadonsgithub <77804816+oaadonsgithub@users.noreply.github.com>
2025-05-04 17:59:23 -0700

    Initial commit for CodeDeploy automation_workflow_6

--- backend.tf
+++ backend.tf
@@ -3,14 +3,14 @@
 
 # The block below configures Terraform to use the 'remote' backend with HCP Terraform.
 # For more information, see https://www.terraform.io/docs/backends/types/remote.html
-terraform {
-  cloud {
-    organization = "oaa_dons"
-
-    workspaces {
-      name = "new_work_place"
-    }
-  }
-
-  required_version = ">= 1.1.2"
-}
+#terraform {
+#  cloud {
+#    organization = "oaa_dons"
+#
+#    workspaces {
+ #     name = "new_work_place"
+#    }
+#  }
+#
+#  required_version = ">= 1.1.2"
+# }
