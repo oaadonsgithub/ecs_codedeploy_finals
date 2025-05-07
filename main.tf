aa8145cbf4e26264d97ca176f4dfb257e316a58a
oaadonsgithub <77804816+oaadonsgithub@users.noreply.github.com>
2025-05-07 16:40:31 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,11 +16,13 @@ terraform {
       version = "~> 2.9.0"
     }
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+    source  = "hashicorp/aws"
+    version = "~> 4.52.0"
     }
   }
 }
+
+
 
 provider "aws" {
   region = var.region
