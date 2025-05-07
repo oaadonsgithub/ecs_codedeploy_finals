fb940ece0927b9fd712380acd65c900925f34f70
oaadonsgithub <77804816+oaadonsgithub@users.noreply.github.com>
2025-05-06 19:07:12 -0700

    app js

---
+++ microservices-app/dashboard/src/index.js
@@ -0,0 +1,12 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
