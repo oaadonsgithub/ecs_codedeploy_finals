ba89a63241c03d1da0ba12d3b887e6eb4a06be69
oaadonsgithub <77804816+oaadonsgithub@users.noreply.github.com>
2025-05-07 17:35:08 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -623,13 +623,13 @@ data "aws_route53_zone" "main" {
 # TLS Key for ACME Account
 
 
-resource "tls_private_key" "acme_account" {
+resource "tls_private_key" "acme_reg" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
 resource "acme_registration" "main" {
-  account_key_pem         = tls_private_key.acme_account.private_key_pem
+  account_key_pem         = tls_private_key.acme_reg.private_key_pem
   email_address           = "admin+new@ianthony.com"  # <- use a new or valid email
 }
 
@@ -638,7 +638,7 @@ resource "acme_registration" "main" {
 # ACME Certificate for Domain
 
 resource "acme_certificate" "cert" {
-  account_key_pem = tls_private_key.acme_account.private_key_pem
+  account_key_pem = tls_private_key.acme_reg.private_key_pem
   common_name     = "karrio.ianthony.com"
 
   dns_challenge {
