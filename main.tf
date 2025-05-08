2a74503e8b712d484620a3f4d8a5da772d0d3a2d
oaadonsgithub <77804816+oaadonsgithub@users.noreply.github.com>
2025-05-08 14:06:13 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -407,6 +407,10 @@ resource "aws_route53_record" "cert_validation" {
 }
 
 
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
 
 
 
