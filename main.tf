daa0769add1101108e59d210e2d8bd547e30191d
oaadonsgithub <77804816+oaadonsgithub@users.noreply.github.com>
2025-05-08 21:09:41 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -628,14 +628,31 @@ resource "aws_lb_target_group" "web_tg" {
 
 
 
-resource "aws_alb_listener" "l_80" {
+#resource "aws_alb_listener" "l_80" {
+#  load_balancer_arn = aws_lb.web_lb.arn
+#  port              = "80"
+#  protocol          = "HTTP"
+#
+#  default_action {
+#    type = "redirect"
+#    target_group_arn = aws_lb_target_group.web_tg[0].arn
+#    redirect {
+#      port        = "443"
+#      protocol    = "HTTPS"
+#      status_code = "HTTP_301"
+#    }
+#  }
+#}
+#
+
+
+resource "aws_alb_listener" "http_redirect" {
   load_balancer_arn = aws_lb.web_lb.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type = "redirect"
-    target_group_arn = aws_lb_target_group.web_tg[0].arn
     redirect {
       port        = "443"
       protocol    = "HTTPS"
@@ -643,6 +660,24 @@ resource "aws_alb_listener" "l_80" {
     }
   }
 }
+
+
+resource "aws_alb_listener" "https_listener" {
+  load_balancer_arn = aws_lb.web_lb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.cert.arn  # ← your ACM cert here
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.fargate_tg.arn
+  }
+}
+
+
+
+
 
 resource "aws_lb_listener" "l_8080" {
   load_balancer_arn = aws_lb.web_lb.id
