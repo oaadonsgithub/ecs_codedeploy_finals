65ed9fc0be391f7d993aee787045bd3ecbc0efbb
oaadonsgithub <77804816+oaadonsgithub@users.noreply.github.com>
2025-05-13 16:38:42 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -624,34 +624,28 @@ resource "aws_lb_target_group" "web_tg" {
 }
 
 
-#resource "aws_lb_listener" "http_listener" {
-#  load_balancer_arn = aws_lb.web_lb.arn
-#  port              = 8081
-#  protocol          = "HTTP"
-#  default_action {
-#    type             = "forward"
-#    target_group_arn = aws_lb_target_group.fargate_tg.arn  
-#  }
-#}
+
+# target group for ECS
+resource "aws_lb_target_group" "fargate_tg_alt" {
+  name        = "fargate-tg-alt"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = var.vpc_id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200-399"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+}
 
 
 
-#resource "aws_alb_listener" "l_80" {
-#  load_balancer_arn = aws_lb.web_lb.arn
-#  port              = "80"
-#  protocol          = "HTTP"
-#
-#  default_action {
-#    type = "redirect"
-#    target_group_arn = aws_lb_target_group.web_tg[0].arn
-#    redirect {
-#      port        = "443"
-#      protocol    = "HTTPS"
-#      status_code = "HTTP_301"
-#    }
-#  }
-#}
-#
 
 
 resource "aws_lb_listener" "http_redirect" {
@@ -684,6 +678,21 @@ resource "aws_lb_listener" "https" {
   }
 }
 
+
+
+resource "aws_lb_listener" "https_alt" {
+  load_balancer_arn = aws_lb.web_alb.arn
+  port              = 8443
+  protocol          = "HTTPS"
+
+  ssl_policy      = "ELBSecurityPolicy-2016-08"
+  certificate_arn = aws_acm_certificate.imported_cert.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.fargate_tg_alt.arn
+  }
+}
 
 
 
