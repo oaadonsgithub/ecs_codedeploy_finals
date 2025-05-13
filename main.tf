2d9f0bb38707fb45fa071b3daf3c2d557034d618
oaadonsgithub <77804816+oaadonsgithub@users.noreply.github.com>
2025-05-13 13:37:41 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -415,21 +415,21 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
-resource "aws_route53_record" "cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      type   = dvo.resource_record_type
-      record = dvo.resource_record_value
-    }
-  }
-
-  zone_id = aws_route53_zone.main.zone_id
-  name    = each.value.name
-  type    = each.value.type
-  records = [each.value.record]
-  ttl     = 60
-}
+#resource "aws_route53_record" "cert_validation" {
+#  for_each = {
+#    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+#      name   = dvo.resource_record_name
+#      type   = dvo.resource_record_type
+#      record = dvo.resource_record_value
+#    }
+#  }
+#
+#  zone_id = aws_route53_zone.main.zone_id
+#  name    = each.value.name
+#  type    = each.value.type
+#  records = [each.value.record]
+#  ttl     = 60
+#}
 
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
