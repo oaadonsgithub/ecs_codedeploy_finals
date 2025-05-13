69179e6aed208e8a55a44f021517ae3fb130e108
oaadonsgithub <77804816+oaadonsgithub@users.noreply.github.com>
2025-05-13 16:57:18 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -580,129 +580,54 @@ locals {
 }
 
 
+########TARGET GROUP START
 
-
-
-# For Fargate ECS service
-resource "aws_lb_target_group" "fargate_tg" {
-  name        = "fargate-tg"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "ip"  
-
-  health_check {
-    path = "/"
-  }
-}
-
-# For EC2 Auto Scaling Group
-resource "aws_lb_target_group" "web_tg" {
-
-  count        = length(local.target_groups)
-
-  name_prefix = "web${count.index}-"
-
-  port        = 5000
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "instance"  
-
-  health_check {
-    path                = "/"
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 5
-    interval            = 10
-    matcher             = "200,301,302,404"
-  }
-
-  lifecycle {
-    # Ensure the name conforms to valid naming rules
-    ignore_changes = [name]
-  }
-}
-
-
-
-# target group for ECS
-resource "aws_lb_target_group" "fargate_tg_alt" {
-  name        = "fargate-tg-alt"
+resource "aws_lb_target_group" "blue" {
+  name        = "blue-tg"
   port        = 80
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = var.vpc_id
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200-399"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 3
-  }
 }
 
-
-
-
-
-resource "aws_lb_listener" "http_redirect" {
-  load_balancer_arn = aws_lb.web_alb.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type = "redirect"
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
+resource "aws_lb_target_group" "green" {
+  name        = "green-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = var.vpc_id
 }
 
-
-resource "aws_lb_listener" "https" {
+resource "aws_lb_listener" "prod_https" {
   load_balancer_arn = aws_lb.web_alb.arn
   port              = 443
   protocol          = "HTTPS"
-
   ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = aws_acm_certificate.imported_cert.arn
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.web_tg[0].arn
+    target_group_arn = aws_lb_target_group.blue.arn
   }
 }
 
 
 
-resource "aws_lb_listener" "https_alt" {
+resource "aws_lb_listener" "test_https" {
   load_balancer_arn = aws_lb.web_alb.arn
   port              = 8443
   protocol          = "HTTPS"
-
-  ssl_policy      = "ELBSecurityPolicy-2016-08"
-  certificate_arn = aws_acm_certificate.imported_cert.arn
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.imported_cert.arn
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.fargate_tg_alt.arn
+    target_group_arn = aws_lb_target_group.green.arn
   }
 }
 
 
 
-
-
-
-resource "aws_autoscaling_attachment" "asg_alb_attachment" {
-  autoscaling_group_name = aws_autoscaling_group.web_asg.name
-  lb_target_group_arn    = aws_lb_target_group.web_tg[local.active_index].arn
-}
 
 resource "aws_iam_user_policy_attachment" "route53_full_access" {
   user       = "oaa_progmatic_01"
@@ -758,102 +683,75 @@ resource "aws_ecs_task_definition" "karrio_task" {
 # ----------------------------
 # 7. ECS Service with CodeDeploy
 # ----------------------------
-resource "aws_ecs_service" "karrio" {
-  name                               = "karrio-service"
-  cluster                            = aws_ecs_cluster.this.id
-  task_definition                    = aws_ecs_task_definition.karrio_task.arn
-  launch_type                        = "FARGATE"
-  desired_count                      = 1
-  platform_version                   = "LATEST"
-  deployment_controller {
-    type = "CODE_DEPLOY"
-  }
 
-  network_configuration {
-    subnets         = var.subnet_ids
-    security_groups = [aws_security_group.web_sg.id]
-    assign_public_ip = true
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.fargate_tg.arn
-    container_name   = "karrio"
-    container_port   = 3000
-  }
-
-  lifecycle {
-    ignore_changes = [
-      task_definition,
-      platform_version,
-      load_balancer
-    ]
-  }
-}
 
 # ----------------------------
 # 8. CodeDeploy Setup
 # ----------------------------
-resource "aws_codedeploy_app" "ecs_app" {
-  name             = "karrio-cd-app"
+resource "aws_codedeploy_app" "ecs" {
+  name             = "karrio-codedeploy"
   compute_platform = "ECS"
 }
 
 resource "aws_codedeploy_deployment_group" "ecs_dg" {
-  app_name               = aws_codedeploy_app.ecs_app.name
-  deployment_group_name  = "karrio-deploy-group"
-  service_role_arn       = "arn:aws:iam::537124950459:role/codedeploy"
+  app_name              = aws_codedeploy_app.ecs.name
+  deployment_group_name = "karrio-deployment-group"
+  service_role_arn      = aws_iam_role.codedeploy_role.arn
   deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
-
-
-  deployment_style {
-    deployment_type   = "BLUE_GREEN"
-    deployment_option = "WITH_TRAFFIC_CONTROL"
-  }
-
-  ecs_service {
-    cluster_name = aws_ecs_cluster.this.name
-    service_name = aws_ecs_service.karrio.name
-  }
-
-  load_balancer_info {
-  target_group_pair_info {
-    target_group {
-      name = aws_lb_target_group.web_tg[0].name
-    }
-    target_group {
-      name = aws_lb_target_group.web_tg[1].name
-    }
-
-    prod_traffic_route {
-      listener_arns = [aws_lb_listener.https.arn]
-    }
-  }
-}
-
 
   auto_rollback_configuration {
     enabled = true
     events  = ["DEPLOYMENT_FAILURE"]
   }
 
-  blue_green_deployment_config {
-    deployment_ready_option {
-      action_on_timeout = "CONTINUE_DEPLOYMENT"
-      wait_time_in_minutes = 0
-    }
+  deployment_style {
+    deployment_type   = "BLUE_GREEN"
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+  }
 
+  blue_green_deployment_config {
     terminate_blue_instances_on_deployment_success {
       action = "TERMINATE"
       termination_wait_time_in_minutes = 5
     }
+
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+    }
+
+    green_fleet_provisioning_option {
+      action = "COPY_AUTO_SCALING_GROUP"
+    }
   }
 
+  ecs_service {
+    cluster_name = aws_ecs_cluster.main.name
+    service_name = aws_ecs_service.app.name
+  }
+
+  load_balancer_info {
+    target_group_pair_info {
+      prod_traffic_route {
+        listener_arns = [aws_lb_listener.prod_https.arn]
+      }
+
+      test_traffic_route {
+        listener_arns = [aws_lb_listener.test_https.arn]
+      }
+
+      target_group {
+        name = aws_lb_target_group.blue.name
+      }
+
+      target_group {
+        name = aws_lb_target_group.green.name
+      }
+    }
+  }
 }
 
-resource "aws_ecr_repository" "web_ecr_repo" {
-  name         = "karrio"
-  force_delete = true
-}
+
+
 
 
 # ----------------------------
