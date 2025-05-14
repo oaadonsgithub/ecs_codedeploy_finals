f7de2881132f2b41b9c1f58ccf413fd08d99577d
oaadonsgithub <77804816+oaadonsgithub@users.noreply.github.com>
2025-05-13 17:02:27 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -684,72 +684,34 @@ resource "aws_ecs_task_definition" "karrio_task" {
 # 7. ECS Service with CodeDeploy
 # ----------------------------
 
+resource "aws_ecs_service" "app" {
+  name            = "karrio-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.karrio.arn
+  launch_type     = "FARGATE"
+
+  deployment_controller {
+    type = "CODE_DEPLOY"
+  }
+
+  network_configuration {
+    subnets         = var.private_subnets
+    security_groups = [aws_security_group.ecs_service.id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.blue.arn
+    container_name   = "karrio"
+    container_port   = 80
+  }
+
+  desired_count = 1
+}
 
 # ----------------------------
 # 8. CodeDeploy Setup
 # ----------------------------
-resource "aws_codedeploy_app" "ecs" {
-  name             = "karrio-codedeploy"
-  compute_platform = "ECS"
-}
-
-resource "aws_codedeploy_deployment_group" "ecs_dg" {
-  app_name              = aws_codedeploy_app.ecs.name
-  deployment_group_name = "karrio-deployment-group"
-  service_role_arn      = aws_iam_role.codedeploy_role.arn
-  deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
-
-  auto_rollback_configuration {
-    enabled = true
-    events  = ["DEPLOYMENT_FAILURE"]
-  }
-
-  deployment_style {
-    deployment_type   = "BLUE_GREEN"
-    deployment_option = "WITH_TRAFFIC_CONTROL"
-  }
-
-  blue_green_deployment_config {
-    terminate_blue_instances_on_deployment_success {
-      action = "TERMINATE"
-      termination_wait_time_in_minutes = 5
-    }
-
-    deployment_ready_option {
-      action_on_timeout = "CONTINUE_DEPLOYMENT"
-    }
-
-    green_fleet_provisioning_option {
-      action = "COPY_AUTO_SCALING_GROUP"
-    }
-  }
-
-  ecs_service {
-    cluster_name = aws_ecs_cluster.main.name
-    service_name = aws_ecs_service.app.name
-  }
-
-  load_balancer_info {
-    target_group_pair_info {
-      prod_traffic_route {
-        listener_arns = [aws_lb_listener.prod_https.arn]
-      }
-
-      test_traffic_route {
-        listener_arns = [aws_lb_listener.test_https.arn]
-      }
-
-      target_group {
-        name = aws_lb_target_group.blue.name
-      }
-
-      target_group {
-        name = aws_lb_target_group.green.name
-      }
-    }
-  }
-}
-
 
 
 
