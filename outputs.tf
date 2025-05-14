5df2706b23e78c714906b5cb49d26a6ac672733b
oaadonsgithub <77804816+oaadonsgithub@users.noreply.github.com>
2025-05-13 17:04:42 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -12,26 +12,6 @@ output "ecs_service_name" {
   value       = aws_ecs_service.karrio.name
 }
 
-output "ecs_task_definition_arn" {
-  description = "ARN of the ECS task definition"
-  value       = aws_ecs_task_definition.karrio_task.arn
-}
-
-
-output "codedeploy_app_name" {
-  description = "CodeDeploy application name"
-  value       = aws_codedeploy_app.ecs_app.name
-}
-
-output "codedeploy_deployment_group_name" {
-  description = "CodeDeploy deployment group name"
-  value       = aws_codedeploy_deployment_group.ecs_dg.deployment_group_name
-}
-
-output "cloudwatch_log_group_name" {
-  description = "Name of the CloudWatch Log Group for ECS"
-  value       = aws_cloudwatch_log_group.karrio.name
-}
 
 
 
